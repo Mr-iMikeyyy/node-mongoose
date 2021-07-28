d6a5cad1d9a511e075ee06126036f47b5ada682e
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-07-28 18:18:08 -0400

    experimenting with server commands

--- index.js
+++ index.js
@@ -8,18 +8,31 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthapizza',
         description: 'test'
-    });
-
-    newDish.save()
-    .then((dish) => {
-        console.log(dish);
-        return Dishes.find({}).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish.toObject());
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: {description: 'Updated test'}
+        },{
+            new: true 
+        }).exec();
+    })
+    .then((dish) => {
+        console.log(dish.toObject());
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'Im getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save()
+    })
+    .then((dish) => {
+        console.log(dish.toObject());
         return Dishes.remove({})
     })
     .then(() => {
